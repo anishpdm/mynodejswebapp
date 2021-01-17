1b17fe3ddfe0b3a3510f83f3b27f2413ecef1514
anishpdm <anish.vilayil.s@gmail.com>
2021-01-17 22:25:04 +0530

    updated mongo staring

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://anish:anishtest@cluster0.zf1or.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology: true });
+//mongoose.connect("mongodb+srv://anish:anishtest@cluster0.zf1or.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/testdb", { useNewUrlParser: true });
 
 
 app.post('/read', async(req,res)=>{
@@ -42,7 +43,7 @@ app.get("/viewall",async(req,res)=>{
 
     try{
        
-        var result= await studModel.find().exec();
+        var result= await studModel.find();
 
         res.json(result);
         
@@ -54,6 +55,35 @@ app.get("/viewall",async(req,res)=>{
     }
 
 })
+
+
+app.post("/search",async(req,res)=>{
+
+    try{
+
+        studModel.find(req.body,  (error,data)=>{
+
+            if(error){
+
+                throw error;
+            }
+            else{
+
+                res.json(data)
+
+            }
+
+        }  )
+
+    }
+    catch(error)
+    {
+        res.status(500).send(error)
+    }
+})
+
+
+
 
 
 app.listen(process.env.PORT || 3000, function() {
